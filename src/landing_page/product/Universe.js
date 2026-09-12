8bee7e44ce1914d6cf8c060c03e6d0f501624057
Shivam Ravikant Kale <shivamrkale1619@gmail.com>
2026-09-12 23:48:28 +0530

    universe

--- src/landing_page/product/Universe.js
+++ src/landing_page/product/Universe.js
@@ -2,60 +2,114 @@ import React from "react";
 
 function Universe() {
   return (
-    <div className="continer mt-5">
+    <div className="container mt-5">
       <div className="row text-center">
-        <p>
-          Extend your trading and investment experience even further with our
-          partner platforms
-        </p>
-        <h1>The Zerodha Universe</h1>
-        <p>
-          Want to know more about our technology stack? Check out the
-          Zerodha.tech blog.
-        </p>
-        <div className="col-4 p-3">
-          <img src="media/images/zerodhafundhouse.png" />
+
+        {/* Heading Section */}
+        <div className="col-12">
           <p>
-            Our asset management venture <br/> that is creating simple and transparent
-            index <br/>funds to help you save for your goals.
+            Extend your trading and investment experience even further with our
+            partner platforms
+          </p>
+
+          <h1>The Zerodha Universe</h1>
+
+          <p>
+            Want to know more about our technology stack? Check out the
+            Zerodha.tech blog.
           </p>
         </div>
-        <div className="col-4 p-3">
-          <img src="media/images/sensibull-logo.png" />
-          <p>
-            Options trading platform that lets you<br/> create strategies, analyze
-            positions, and examine <br/>data points like open interest, FII/DII, and
-            more.
+
+        {/* Zerodha Fund House */}
+        <div className="col-4 p-3 mt-5">
+          <img
+            src="media/images/zerodhafundhouse.png"
+            alt="Zerodha Fund House"
+            style={{ maxWidth: "200px" }}
+          />
+
+          <p className="text-small text-muted mt-3">
+            Our asset management venture <br />
+            that is creating simple and transparent index <br />
+            funds to help you save for your goals.
           </p>
         </div>
+
+        {/* Sensibull */}
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/tijori.png" />
-          <p>
-            Investment research platform<br/> that offers detailed insights on
-            stocks, <br/>sectors, supply chains, and more.
+          <img
+            src="media/images/sensibullLogo.png"
+            alt="Sensibull"
+            style={{ maxWidth: "200px" }}
+          />
+
+          <p className="text-small text-muted mt-3">
+            Options trading platform that lets you <br />
+            create strategies, analyze positions, and examine <br />
+            data points like open interest, FII/DII, and more.
           </p>
         </div>
+
+        {/* Tijori */}
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/streak-logo.png" />
+          <img
+            src="media/images/tijori.png"
+            alt="Tijori"
+            style={{ maxWidth: "200px" }}
+          />
+
+          <p className="text-small text-muted mt-3">
+            Investment research platform <br />
+            that offers detailed insights on stocks, <br />
+            sectors, supply chains, and more.
+          </p>
         </div>
-        <p>
-          Systematic trading platform <br/>that allows you to create and backtest<br/>
-          strategies without coding.
-        </p>
+
+        {/* Streak */}
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcase-logo.png" />
-          <p>
-            Thematic investing platform <br/>that helps you invest in diversified<br/>
+          <img
+            src="media/images/streakLogo.png"
+            alt="Streak"
+            style={{ maxWidth: "200px" }}
+          />
+
+          <p className="text-small text-muted mt-3">
+            Systematic trading platform <br />
+            that allows you to create and backtest <br />
+            strategies without coding.
+          </p>
+        </div>
+
+        {/* Smallcase */}
+        <div className="col-4 p-3 mt-5">
+          <img
+            src="media/images/smallcaseLogo.png"
+            alt="Smallcase"
+            style={{ maxWidth: "200px" }}
+          />
+
+          <p className="text-small text-muted mt-3">
+            Thematic investing platform <br />
+            that helps you invest in diversified <br />
             baskets of stocks on ETFs.
           </p>
         </div>
+
+
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/ditto-logo.png" />
-          <p>
-            Personalized advice on life<br/> and health insurance. No spam<br/> and no
-            mis-selling.
+          <img
+            src="media/images/dittoLogo.png"
+            alt="Ditto"
+            style={{ maxWidth: "200px" }}
+          />
+
+          <p className="text-small text-muted mt-3">
+            Personalized advice on life <br />
+            and health insurance. No spam <br />
+            and no mis-selling.
           </p>
         </div>
+
       </div>
     </div>
   );
